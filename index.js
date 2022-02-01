4883ac7c00e022cb7d7f950f921d44c995f1d212
yuvarani03 <yuvarvh1998@gmail.com>
2022-02-01 12:26:42 +0530

    done

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
 class SortedList {
   constructor() {
-  
+    this.items = []
+    this.length = 0
   }
-  add() {
-   
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b)=>(a-b))
+    this.length = this.length+1;
   }
-  get() {
 
+  get(value) {
+    if(this.items.indexOf(value) == -1){
+      throw new Error('OutOfBounds')
+    }
+    else{
+      return(this.items.indexOf(value))
+    }
   }
+
   max() {
-
+    if(this.items.length >0){
+      return this.items[this.items.length - 1]
+    }
+    else{
+      throw new Error('EmptySortedList')
+    }
   }
+
   min() {
-
+    if(this.items.length > 0){
+      return this.items[0]
+    }
+    else{
+      throw new Error('EmptySortedList')
+    }
   }
+
   avg() {
-    
-   
+    if(this.items.length > 0){
+      return this.sum() / this.items.length
+    }
+    else{
+      throw new Error('EmptySortedList')
+    }
   }
 
   sum() {
-  
+    if(this.items.length>0){
+      return this.items.reduce((a,b)=> {
+        return a +b;}, 0)
+    }
+    else{
+      return 0
+    }
   }
-  
 };
 
 module.exports = SortedList;
